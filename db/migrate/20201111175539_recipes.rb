3bd60302dd2b64879b3502ff2b5cf36eab4a5018
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-11-11 18:04:07 +0000

    Automatically backed up by Learn

--- db/migrate/20201111175539_recipes.rb
+++ db/migrate/20201111175539_recipes.rb
@@ -1,4 +1,10 @@
 class Recipes < ActiveRecord::Migration
   def change
+    create_table :recipes do |recipe|
+      recipe.string :name
+      recipe.string :ingredients
+      recipe.string :cook_time
+      
+    end
   end
 end
