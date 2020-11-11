4f930031d4d41c80feb44238074adfa9c36418a9
Daniel Hoyte <daniel.hoyte59@gmail.com>
2020-11-11 22:52:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,13 +12,13 @@ class ApplicationController < Sinatra::Base
     @recipes = Recipe.all
     erb :index
   end
-  
+
   get '/recipes/new' do
     erb  :new
   end
   post '/recipes' do
      @recipe = Recipe.create(params)
- 
+
      redirect to "/recipes/#{@recipe.id}"
    end
 
